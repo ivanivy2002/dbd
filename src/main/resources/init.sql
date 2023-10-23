ea076f39e8764418c94369c26e255035336a2268
ivan <2410297162@qq.com>
2023-10-24 01:18:38 +0800

    dbdlab1-code-fin

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,31 +1,22 @@
-CREATE TABLE IF NOT EXISTS room (
-    kdno INT,
-    kcno INT,
-    ccno INT,
-    kdname VARCHAR(255),
-    exptime TIMESTAMP,
-    papername VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS student (
-    registno VARCHAR(10),
-    name VARCHAR(255),
-    kdno INT,
-    kcno INT,
-    ccno INT,
-    seat INT
-);
-
-LOAD DATA INFILE '../../../data/room.csv'
-INTO TABLE room
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
-LOAD DATA INFILE '../../../data/student.csv'
-INTO TABLE room
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
+drop table if exists room;
+create table if not exists room
+(
+    kdno      int(4),
+    kcno      int(2),
+    ccno      int(2),
+    kdname    varchar(20),
+    exptime   timestamp,
+    papername varchar(10),
+    primary key (kdno, kcno, ccno)
+) DEFAULT CHARACTER SET utf8;
+drop table if exists student;
+create table if not exists student
+(
+    registno varchar(7),
+    name     varchar(20),
+    kdno     int(4),
+    kcno     int(2),
+    ccno     int(2),
+    seat     int(1),
+    primary key (registno)
+) DEFAULT CHARACTER SET utf8;
